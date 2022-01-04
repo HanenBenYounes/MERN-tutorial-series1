d72b8ecbeccdbbca842992cb957a0f690773a627
HanenBenYounes <hanen.benaliessalah@gmail.com>
2022-01-04 17:18:23 +0100

    initialized our frontend and backend

---
+++ backend/data/notes.js
@@ -0,0 +1,36 @@
+const notes = [
+    {
+        _id: "1",
+        title: "Day of college",
+        content:
+            "I made a few new friends and introduced myself to a lot of new teachers.",
+        Category: "college",
+    },
+    {
+        _id: "2",
+        title: "Learned some Node JS",
+        
+        content:
+            "Learned how to create a server node JS and a self API",
+        Category: "learning",
+    },
+    {
+        _id: "3",
+        title: "watching some movies",
+        
+        content:
+            "watching some movies in order to entertain our selves",
+        Category: "Entertainment",
+    },
+    {
+        _id: "4",
+        title: "Started react JS",
+        
+        content:
+            "Learning how to build a react js App ",
+        Category: "Learning",
+    },
+
+
+];
+module.exports = notes;
